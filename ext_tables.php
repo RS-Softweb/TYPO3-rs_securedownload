6eaadeac1dace2d801ee2a483b168edd26f44663
SvenJuergens <sj@nordsonne.de>
2017-02-27 13:39:53 +0100

    [TASK] Disable BackendModule at the moment

--- ext_tables.php
+++ ext_tables.php
@@ -2,8 +2,13 @@
 defined('TYPO3_MODE') or die();
 
 
-if (TYPO3_MODE == 'BE') {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('web', 'txrssecuredownloadM1', '', \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'mod1/');
-}
+#if (TYPO3_MODE == 'BE') {
+#    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
+#        'web',
+#        'txrssecuredownloadM1',
+#        '',
+#        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'mod1/'
+#    );
+#}
 
 
