3e317cec8a092185ec169ed2322195f84b89917e
SvenJuergens <sj@nordsonne.de>
2017-02-27 11:51:20 +0100

    [CLEANUP] PSR2

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,16 +1,21 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
 
-use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43(
+    $_EXTKEY,
+    'pi1/class.tx_rssecuredownload_pi1.php',
+    '_pi1',
+    'list_type',
+    0
+);
 
-// Extending TypoScript from static template uid=43 to set up userdefined tag:
-ExtensionManagementUtility::addTypoScript($_EXTKEY, 'editorcfg', 
-	'tt_content.CSS_editor.ch.tx_rssecuredownload_pi1 = < plugin.tx_rssecuredownload_pi1.CSS_editor', 43);
-
-ExtensionManagementUtility::addPItoST43($_EXTKEY, 'pi1/class.tx_rssecuredownload_pi1.php', '_pi1', 'list_type', 0);
-
-ExtensionManagementUtility::addTypoScript($_EXTKEY, 'setup', '
-	tt_content.shortcut.20.0.conf.tx_rssecuredownload_codes = < plugin.'.ExtensionManagementUtility::getCN($_EXTKEY).'_pi1
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+    $_EXTKEY,
+    'setup', '
+	tt_content.shortcut.20.0.conf.tx_rssecuredownload_codes = < plugin.' . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getCN($_EXTKEY) . '_pi1
 	tt_content.shortcut.20.0.conf.tx_rssecuredownload_codes.CMD = singleView
-', 43);
-?>
+    ',
+    43
+);
--- pi1/class.tx_rssecuredownload_pi1_wizicon.php
+++ pi1/class.tx_rssecuredownload_pi1_wizicon.php
@@ -25,7 +25,6 @@
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  */
-
 use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use \TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -33,42 +32,38 @@ use \TYPO3\CMS\Core\Utility\GeneralUtility;
  * Class that adds the wizard icon.
  *
  * @author	Rene <typo3@rs-softweb.de>
- * @package	TYPO3
- * @subpackage	tx_rssecuredownload
  */
-class tx_rssecuredownload_pi1_wizicon {
+class tx_rssecuredownload_pi1_wizicon
+{
 
-	/**
-	 * Processing the wizard items array
-	 *
-	 * @param	array		$wizardItems: The wizard items
-	 * @return	Modified array with wizard items
-	 * @access	public
-	 */
-	public function proc($wizardItems)	{
-		$LL = $this->includeLocalLang();
-		$wizardItems['plugins_tx_rssecuredownload_pi1'] = array(
-			'icon'=>ExtensionManagementUtility::extRelPath('rs_securedownload').'pi1/ce_wiz.gif',
-			'title'=>$GLOBALS['LANG']->getLLL('pi1_title', $LL),
-			'description'=>$GLOBALS['LANG']->getLLL('pi1_plus_wiz_description', $LL),
-			'params'=>'&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=rs_securedownload_pi1'
-		);
-		return $wizardItems;
-	}
+    /**
+     * Processing the wizard items array
+     *
+     * @param	array		$wizardItems: The wizard items
+     * @return	Modified array with wizard items
+     * @access	public
+     */
+    public function proc($wizardItems)
+    {
+        $LL = $this->includeLocalLang();
+        $wizardItems['plugins_tx_rssecuredownload_pi1'] = [
+            'icon'=>ExtensionManagementUtility::extRelPath('rs_securedownload') . 'pi1/ce_wiz.gif',
+            'title'=>$GLOBALS['LANG']->getLLL('pi1_title', $LL),
+            'description'=>$GLOBALS['LANG']->getLLL('pi1_plus_wiz_description', $LL),
+            'params'=>'&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=rs_securedownload_pi1'
+        ];
+        return $wizardItems;
+    }
 
-	/**
-	 * Reads the [extDir]/locallang.xml and returns the $LOCAL_LANG array found in that file.
-	 *
-	 * @return	The array with language labels
-	 */
-	private function includeLocalLang()	{
-		$llFile = ExtensionManagementUtility::extPath('rs_securedownload').'locallang.xml';
-		$LOCAL_LANG =  GeneralUtility::readLLfile($llFile, $GLOBALS['LANG']->lang);
-		return $LOCAL_LANG;
-	}
+    /**
+     * Reads the [extDir]/locallang.xml and returns the $LOCAL_LANG array found in that file.
+     *
+     * @return	The array with language labels
+     */
+    private function includeLocalLang()
+    {
+        $llFile = ExtensionManagementUtility::extPath('rs_securedownload') . 'locallang.xml';
+        $LOCAL_LANG =  GeneralUtility::readLLfile($llFile, $GLOBALS['LANG']->lang);
+        return $LOCAL_LANG;
+    }
 }
-
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rs_securedownload/pi1/class.tx_rssecuredownload_pi1_wizicon.php'])	{
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rs_securedownload/pi1/class.tx_rssecuredownload_pi1_wizicon.php']);
-}
-?>
