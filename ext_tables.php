6ad069175385daef3e84502450550f311021b537
SvenJuergens <sj@nordsonne.de>
2017-02-27 15:57:16 +0100

    [TASK] allowTableOnStandardPages

--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,7 @@
 <?php
 defined('TYPO3_MODE') or die();
 
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rssecuredownload_codes');
 
 #if (TYPO3_MODE == 'BE') {
 #    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
