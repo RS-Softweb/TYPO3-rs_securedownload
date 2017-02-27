200a9af02c9533e12f351acf6d7ca62ccf0441f1
SvenJuergens <sj@nordsonne.de>
2017-02-27 13:41:08 +0100

    [Bugfix] Path to IconFile

--- Configuration/PageTS/NewContentElementWizard.ts
+++ Configuration/PageTS/NewContentElementWizard.ts
@@ -3,7 +3,7 @@ mod.wizards {
         plugins {
             elements {
                 plugins_tx_rssecuredownload_pi1 {
-                    icon = EXT:rs_securedownload//Resources/Public/Icons/ce_wiz.gif
+                    icon = EXT:rs_securedownload/Resources/Public/Icons/ce_wiz.gif
                     title = LLL:EXT:rs_securedownload/Resources/Private/Language/locallang.xml:pi1_title
                     description = LLL:EXT:rs_securedownload/Resources/Private/Language/locallang.xml:pi1_plus_wiz_description
                     tt_content_defValues {
